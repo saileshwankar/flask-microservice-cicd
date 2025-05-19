5cc83775489bc8da8e18bf2b4b155ac1d7fc0387
saileshwankar9 <saileshwankar9@gmail.com>
2025-05-20 00:50:09 +0530

    sailesh : test weeb hook test 1

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ def home():
     return "Hello from Flask Microservice!"
 
 
-@app.route('/getdata')
-def home1():
-    return "Hello from Flask Microservice for get data!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
