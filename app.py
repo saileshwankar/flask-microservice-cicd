7fab8aea3923a0fdc542a02c466192bda286c195
Sailesh Wankar <96332037+saileshwankar@users.noreply.github.com>
2025-07-17 00:21:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
+
+
+env = os.environ.get("ENV")  # Will return "prod"
 
 @app.route('/')
 def home():
     return "Hello from Flask Microservice!"
 
-
+@app.route("/env")
+def show_env():
+    return f"ENV: {os.getenv('ENV')}"
 
 
 if __name__ == '__main__':
